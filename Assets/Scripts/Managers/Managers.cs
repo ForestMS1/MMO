7282799b993c5e345960a1e350fa3adfa012aaf9
daesung <dasung56@gmail.com>
2025-03-05 19:26:48 +0900

    Managers 싱글톤 적용

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    static Managers Instance; //유일성 보장
-    public static Managers GetInstance() { Init(); return Instance; }
-    
-    // Start is called before the first frame update
+    static Managers s_instance; //유일성 보장 (전역 변수)
+
+    public static Managers Instance { get { Init(); return s_instance; } } //유일한 매니저를 갖고온다 (프로퍼티)
+
     void Start()
     {
         Init();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
-
+    
+    //싱글톤 패턴으로 매니저는 한개만 존재
     static void Init()
     {
-        if (Instance == null)
+        if (s_instance == null)
         {
             GameObject go = GameObject.Find("@Managers");
             if (go == null)
@@ -29,8 +29,8 @@ public class Managers : MonoBehaviour
                 go = new GameObject("@Managers");
                 go.AddComponent<Managers>();
             }
-            DontDestroyOnLoad(go);
-            Instance = go.GetComponent<Managers>();
+            DontDestroyOnLoad(go); //중요한 오브젝트가 지워지지 않도록 설정
+            s_instance = go.GetComponent<Managers>();
         }
     }
 }
